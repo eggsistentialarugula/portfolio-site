99e4310a7e34ca1aa9cbf3bd1062e6b45dad5d5a
tadpoleann <laesybab@gmail.com>
2021-12-29 06:07:47 -0800

    Created contact form and added a lot of styles to it

--- script.js
+++ script.js
@@ -31,3 +31,16 @@ const typeAniText = document.querySelector(".top-container h1");
 window.onload = () => {
   typeAniText.classList.add("greeting");
 };
+
+// contact form
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "username",
+    Password: "password",
+    To: "them@website.com",
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
